735d23eac78d8428e494ac0f11ca21cb095325d5
Ruben Krueger <rubenkrueger99@gmail.com>
2025-07-14 15:55:39 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <SidebarProvider>
           <AppSideBar />
-          <div className="min-h-screen bg-gray-50 flex-1 flex-col">
+          <div className="min-h-screen bg-gray-50 flex-1 flex flex-col">
             <Header />
             <main className="flex-1 py-8">
               <Routes>
